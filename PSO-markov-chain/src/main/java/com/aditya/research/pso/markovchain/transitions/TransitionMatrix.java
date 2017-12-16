fbf463741c36c222ec8915b69c6a011daec22c96
aditya <aditya.s16@gmail.com>
2017-12-15 22:16:13 -0500

    deleting old version

--- PSO-markov-chain/src/main/java/com/aditya/research/pso/markovchain/transitions/TransitionMatrix.java
+++
@@ -1,7 +0,0 @@
-package com.aditya.research.pso.markovchain.transitions;
-
-import com.aditya.research.pso.markovchain.states.State;
-
-public interface TransitionMatrix {
-	float getTransitionProbability(State state,boolean isAShot);
-}
