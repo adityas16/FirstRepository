68c33fc372dd89c7745ed3df7dc9b19a57f15859
aditya <aditya.s16@gmail.com>
2017-12-19 23:20:12 -0500

    using edges instead of nodes to compute binomial and trinomial probs

--- R_Scripts/PSO/PairWiseTransitions.R
+++ R_Scripts/PSO/PairWiseTransitions.R
@@ -22,34 +22,35 @@ create_pairwise_transitions_table = function(shootouts){
   q=ddply(shootouts,c("round_adjusted","gd","is_team_A_shot"),p=myprop(isConverted),n=length(uri),summarise)
   
   k=sqldf("select k.*,q.p as q_s_1 from k,q where 
-      k.round_adjusted=q.round_adjusted 
-        and k.gd = q.gd
-        and is_team_A_shot = 1")    
+          k.round_adjusted=q.round_adjusted 
+          and k.gd = q.gd
+          and is_team_A_shot = 1")    
   
   k=sqldf("select k.*,q.p as q_s_2 from k left join q on 
-        k.round_adjusted=q.round_adjusted 
-        and k.gd = q.gd
-        and is_team_A_shot = 0")    
+          k.round_adjusted=q.round_adjusted 
+          and k.gd = q.gd
+          and is_team_A_shot = 0")    
   
   k=sqldf("select k.*,q.p as q_splus1_2 from k left join q on 
-        k.round_adjusted=q.round_adjusted 
-        and k.gd + 1 = q.gd
-        and is_team_A_shot = 0")    
+          k.round_adjusted=q.round_adjusted 
+          and k.gd + 1 = q.gd
+          and is_team_A_shot = 0")    
   
   k=sqldf("select k.*,q.n as n_s_2 from k left join q on 
-        k.round_adjusted=q.round_adjusted 
-        and k.gd = q.gd
-        and is_team_A_shot = 0")    
+          k.round_adjusted=q.round_adjusted 
+          and k.gd = q.gd
+          and is_team_A_shot = 0")    
   
   k=sqldf("select k.*,q.n as n_splus1_2 from k left join q on  
-        k.round_adjusted=q.round_adjusted 
-        and k.gd + 1= q.gd
-        and is_team_A_shot = 0")    
+          k.round_adjusted=q.round_adjusted 
+          and k.gd + 1= q.gd
+          and is_team_A_shot = 0")    
   
   
   colnames(k)[1] = "j"
   colnames(k)[2] = "s"
   colnames(k)[3] = "c"
+  k=k[order(k$j,k$s * -1,k$c * -1),]
   return(k)
 }
 
@@ -78,23 +79,110 @@ filtered_games = data.frame(pso[pso$is_last_shot==1,c("uri")])
 colnames(filtered_games)=c("uri")
 
 k=data.frame()
-for(i in 1:10){
-itertation_games = data.frame(filtered_games[sample(1:nrow(filtered_games),replace = T),])
-colnames(itertation_games)=c("uri")
-itertation_games$unique_game_id = 1:nrow(itertation_games)
-
-iteration_shootouts = myjoin(pso,itertation_games,join_type="")
-#iteration_k=create_pairwise_transitions_table(iteration_shootouts)
-iteration_k=binomial_transitions_table(iteration_shootouts)
-iteration_k$iteration_number = i
-k=rbind(k,iteration_k)
+set.seed(42);
+iteration_seeds = sample(1000000,size=10000)
+for(i in 1:1000){
+  set.seed(iteration_seeds[i])
+  itertation_games = data.frame(filtered_games[sample(1:nrow(filtered_games),replace = T),])
+  colnames(itertation_games)=c("uri")
+  itertation_games$unique_game_id = 1:nrow(itertation_games)
+  
+  iteration_shootouts = myjoin(pso,itertation_games,join_type="")
+  iteration_k=create_pairwise_transitions_table(iteration_shootouts)
+  #iteration_k=create_binomial_transitions_table(iteration_shootouts)
+  iteration_k$iteration_number = i
+  k=rbind(k,iteration_k)
+  print(i)
 }
 k=k[,c(length(k),1:length(k-1))]
 k$iteration_number.1=NULL
-to_csv(k[order(k$iteration_number,k$j,k$s * -1,k$c * -1),])
+to_csv(k)
 
 
 #Testing bootstrapping code
 x=read.csv("/home/aditya/Dropbox/penalties/Analysis/By Round/PairWiseTransitions/PairwiseTransitions_Bootstrap_AER_20171127.csv")
 y=ddply(x,c("j","s","c"),tr_pr=mean(tr_probability),qs_1=mean(q_s_1),n=mean(n),summarize)
 to_csv(y[order(y$j,y$s * -1,y$c * -1),])
+
+
+#Different approach, counting edges instead of nodes
+#append previous shot result
+pso$is_prev_converted = c(0,head(pso$isConverted,-1))
+pso$is_prev_converted[pso$isFirstShot==1]=0
+
+
+count_edges=function(team_A_shot,
+                     converted,
+                     prev_converted){
+return(ddply(pso[pso$is_team_A_shot==team_A_shot & pso$isConverted==converted & pso$is_prev_converted == prev_converted,],
+             c("round_adjusted","gd"),n=length(uri),summarise))
+}
+
+append_count = function(x,y,colname = "n1a"){
+x=sqldf("select x.*,y.n from x left join y on x.round_adjusted = y.round_adjusted and x.gd=y.gd")
+x$n[is.na(x$n)]=0
+colnames(x)[length(colnames(x))] = colname
+return (x)
+}
+
+x=ddply(pso[pso$is_team_A_shot==1,],c("round_adjusted","gd"),n0=length(uri),summarise)
+
+x=append_count(x,count_edges(1,1,0),"n1a")
+x=append_count(x,count_edges(1,1,1),"n1b")
+x$n1=x$n1a+x$n1b
+x$n1a=NULL
+x$n1b=NULL
+
+x=append_count(x,count_edges(1,0,0),"n2a")
+x=append_count(x,count_edges(1,0,1),"n2b")
+x$n2=x$n2a+x$n2b
+x$n2a=NULL
+x$n2b=NULL
+
+
+x=append_count(x,count_edges(0,0,0),"n6")
+x=append_count(x,count_edges(0,1,0),"n5")
+
+x=append_count(x,count_edges(0,0,1),"n12")
+x=append_count(x,count_edges(0,1,1),"n11")
+
+y=count_edges(0,0,1)
+y$gd = y$gd -1
+x=append_count(x,y,"n4")
+y=count_edges(0,1,1)
+y$gd = y$gd -1
+x=append_count(x,y,"n3")
+
+y=count_edges(0,0,0)
+y$gd = y$gd -1
+x=append_count(x,y,"n10")
+y=count_edges(0,1,0)
+y$gd = y$gd -1
+x=append_count(x,y,"n9")
+
+#tests
+x$n1+x$n2 - x$n0
+x$n4+x$n3 - x$n1
+x$n5+x$n6 - x$n2
+
+x$n_tri_up = x$n4
+x$n_tri_0 = x$n3+x$n6
+x$n_tri_0[x$n3==0 | x$n6==0]=0
+x$n_tri_down = x$n5
+x$has_trinomial=(x$n_tri_up !=0 & x$n_tri_down !=0 & x$n_tri_0 !=0)
+tri_total = x$n_tri_up + x$n_tri_down + x$n_tri_0
+x$tri_up = x$n_tri_up / tri_total
+x$tri_0 = x$n_tri_0 / tri_total
+x$tri_down = x$n_tri_down / tri_total
+
+
+x$p_s = x$n1/x$n0
+x$q_s = x$n5/(x$n5 + x$n6)
+x$q_splus1 = x$n3/(x$n3 + x$n4)
+
+x$q_prime_s = (x$n11 + x$n5)/(x$n5 + x$n6 + x$n11 + x$n12)
+x$q_prime_splus1 = (x$n3 + x$n9)/(x$n3 + x$n4 + x$n9 + x$n10)
+
+x$e2 = x$n4 + x$n3
+x$e3 = x$n5 + x$n6
+
