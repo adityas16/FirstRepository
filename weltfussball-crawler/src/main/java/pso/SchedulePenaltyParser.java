85a72cc11cc8b7e954e7371ed09b9bbec26cd3aa
adityas16 <aditya.s16@gmail.com>
2019-09-13 17:52:25 -0400

    Using team IDS to identify games
    Added code to parse team IDS, so it can be used to create meaningful
    gameids for games that don't have their own URI

--- weltfussball-crawler/src/main/java/pso/SchedulePenaltyParser.java
+++ weltfussball-crawler/src/main/java/pso/SchedulePenaltyParser.java
@@ -19,13 +19,12 @@ import com.aditya.research.pso.parsers.Parser;
 
 
 public class SchedulePenaltyParser implements Parser {
-	static int gameWithoutLinkCounter = 1;
-//	static FileSystemCache webPageCache = new FileSystemCache("/home/aditya/Research Data/weltfussball/seasons/", Constants.baseURL + Constants.all_matches + "/");
+	//	static FileSystemCache webPageCache = new FileSystemCache("/home/aditya/Research Data/weltfussball/seasons/", Constants.baseURL + Constants.all_matches + "/");
 	DBCache webPageCache = DBCache.weltseasonCache();
-	
+
 	public  List<Map<String, String>> parse(Document doc){
-		//return parseAllShootouts(doc);
-		return parseShootoutsWithLink(doc);
+		return parseAllShootouts(doc);
+		//		return parseShootoutsWithLink(doc);
 	}
 
 	private List<Map<String, String>> parseShootoutsWithLink(Document doc) {
@@ -56,59 +55,98 @@ public class SchedulePenaltyParser implements Parser {
 				competitionName = m.group(1);
 			}
 		}
+		String competitionID="NA";
+		m = Pattern.compile("/competition/([^/]*)/").matcher(doc.select("#navi > div.sitenavi > div > div > ul:nth-child(1) > li > a").get(0).outerHtml());
+		if(m.find()) {
+			competitionID = m.group(1);
+		}
 		
+		
+		m = Pattern.compile("class=\"sitenavi_active\" href=\"/all_matches/(.*)/\">Schedule</a></li>").matcher(doc.outerHtml());
+		String season_uri="";
+		if(m.find()) {
+			season_uri = m.group(1);
+		}
+		else {
+			throw new RuntimeException("Could not find season URI");
+		}
+
 		Node table = doc.getElementsByAttributeValue("class", "portfolio").get(0).getElementsByAttributeValue("class", "box").get(0).getElementsByClass("standard_tabelle").get(0).childNode(1);
-		
+
 		Iterator<Node> rowIterator = table.childNodes().iterator();
 		while (rowIterator.hasNext()) {
-						rowIterator.next();
+			rowIterator.next();
 			if(!rowIterator.hasNext()) {
 				break;
 			}
 			Node row = rowIterator.next();
-			
+
 			m = Pattern.compile("title=\"further matches on (\\d+)/(\\d+)/(\\d+)\"").matcher(row.outerHtml());
 			if(m.find()) {
 				latestDay = m.group(1);
 				latestMonth = m.group(2);
 				latestYear = m.group(3);
 			}
-			
+
 			if(!row.outerHtml().contains("pso")) {
 				continue;
 			}
-			
+
+			//Only needed for aet
+//			if(!row.childNodes().get(11).outerHtml().contains("aet")) {
+//				continue;
+//			}
+
 			Map<String, String> asMap = create_game_map();
-			
+			asMap.put("competition", competitionName);
+			asMap.put("competition_ID", competitionID);
+
 			asMap.put("day", latestDay);
 			asMap.put("month", latestMonth);
 			asMap.put("year", latestYear);
+			
+			m = Pattern.compile("/schedule/.*\">(.*)</a").matcher(row.outerHtml());
+			if(m.find()) {
+				asMap.put("round", m.group(1));
+			}
+
+			m = Pattern.compile("<a href=\"/teams/.* title=\"(.*)\\\"").matcher(row.outerHtml());
+			if(m.find()) {
+				asMap.put("homeTeam", m.group(1));
+			}
+			if(m.find()) {
+				asMap.put("awayTeam", m.group(1));
+			}
+			
+			m = Pattern.compile("align=\"right\"><a href=\"/teams/([^/]*)").matcher(row.outerHtml());
+			if(m.find()) {
+				asMap.put("homeTeamID", m.group(1));
+			}
+			
+			m = Pattern.compile("<a href=\"/teams/([^/]*)/\" title=.*<a href=\\\"/teams/([^/]*)").matcher(row.outerHtml().replace("\n", "").replace("\r", ""));
+			if(m.find()) {
+				asMap.put("awayTeamID", m.group(2));
+			}
 			
 			m = Pattern.compile("<a href=\"/report/(.*)/\"").matcher(row.outerHtml());
 			if(m.find()) {
 				asMap.put("uri" , m.group(1));
 			}
 			else {
-				asMap.put("uri" , "welt_no_seq_" + (gameWithoutLinkCounter ++));
+				/*If the match does not have it own page/uri, create a URI as:
+				<season_uri>_<yearmonthday>_<home_team_id><away_teamid>
+				If there are no team IDs , use names instead:<season_uri>_<yearmonthday>_<home_team_name><away_team name>*/
+				if(!asMap.get("homeTeamID").equals("NA") && !asMap.get("awayTeamID").equals("NA")) {
+					asMap.put("uri" , "myid_"+season_uri+"_"+latestYear+""+latestMonth +"" +latestDay +"_" + asMap.get("homeTeamID")+"_" + asMap.get("awayTeamID"));
+				}
+				else {
+					throw new RuntimeException("No Team IDs found for shootout game");
+//					asMap.put("uri" , "welt_no_seq_" + (gameWithoutTeamIDCounter ++));
+				}
+				
 			}
 			
-			asMap.put("competition", competitionName);
-			
-			
-			m = Pattern.compile("/schedule/.*\">(.*)</a").matcher(row.outerHtml());
-			if(m.find()) {
-				asMap.put("round", m.group(1));
-			}
-			 
-			 m = Pattern.compile("<a href=\"/teams/.* title=\"(.*)\\\"").matcher(row.outerHtml());
-			 if(m.find()) {
-				 asMap.put("homeTeam", m.group(1));
-			 }
-			 if(m.find()) {
-				 asMap.put("awayTeam", m.group(1));
-			 }
-			 records.add(asMap);
-			 System.out.println(asMap);
+			records.add(asMap);
 		}
 		return records;
 	}
@@ -125,13 +163,15 @@ public class SchedulePenaltyParser implements Parser {
 		asMap.put("season", "NA");
 		asMap.put("round", "NA");
 		asMap.put("dist_from_final", "NA");
-		//asMap.put("competition_ID","NA");
+		asMap.put("competition_ID","NA");
 		asMap.put("day", "NA");
 		asMap.put("month", "NA");
 		asMap.put("year", "NA");
+		asMap.put("homeTeamID", "NA");
+		asMap.put("awayTeamID", "NA");
 		return asMap;
 	}
-	
+
 	public List<String> parseURI(String uri) throws IOException{
 		List<Map<String, String>> records = parse(webPageCache.get(uri));
 		List<String> uris = new ArrayList<String>();
@@ -140,21 +180,19 @@ public class SchedulePenaltyParser implements Parser {
 		}
 		return uris;
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		SchedulePenaltyParser schedulePenaltyParser = new SchedulePenaltyParser();
-//		System.out.println(
-//				schedulePenaltyParser.parseURI("aze-kubok-2013-2014")
-//				);
-//		System.out.println(
-//				schedulePenaltyParser.parseURI("wm-2014-in-brasilien")
-//				);
-		
-		System.out.println(
-				schedulePenaltyParser.parseURI("ec-der-pokalsieger-1995-1996")
-				);
+		//		System.out.println(
+		//				schedulePenaltyParser.parseURI("aze-kubok-2013-2014")
+		//				);
+		//		System.out.println(
+		//				schedulePenaltyParser.parseURI("wm-2014-in-brasilien")
+		//				);
 
-		
+		System.out.println(
+				schedulePenaltyParser.parseURI("gold-cup-2019")
+				);
 	}
 }
 
